28e7cfa49ba455a0ea45eeecc80d317c1376394a
mahimsafa <mahimsafa@gmail.com>
2025-06-02 00:47:04 +0600

    escapped special character

--- setup_server.sh
+++ setup_server.sh
@@ -144,12 +144,12 @@ server {
         proxy_cache_bypass \$http_upgrade;
         client_max_body_size 500M;
         
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Forwarded-Proto $scheme;
-        proxy_set_header X-Forwarded-Host $host;
-        proxy_set_header X-Forwarded-Port $server_port;
-        proxy_set_header X-Forwarded-Server $host;
-        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_set_header X-Forwarded-Host \$host;
+        proxy_set_header X-Forwarded-Port \$server_port;
+        proxy_set_header X-Forwarded-Server \$host;
+        proxy_set_header X-Forwarded-Proto \$scheme;
         proxy_redirect off;
 
     }
